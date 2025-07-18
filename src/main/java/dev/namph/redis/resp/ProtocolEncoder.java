d62be4d963f213310d80b253e42661350861b707
namphn <hoangnamuet.vnu@gmail.com>
2025-07-18 10:11:22 +0700

    feat: define protocol encoder interface

--- src/main/java/dev/namph/redis/resp/ProtocolEncoder.java
+++ src/main/java/dev/namph/redis/resp/ProtocolEncoder.java
@@ -1,4 +1,35 @@
 package dev.namph.redis.resp;
 
 public interface ProtocolEncoder {
+    /**
+     * Encodes an error message into the RESP protocol format.
+     *
+     * @param errorMessage The error message to encode.
+     * @return The encoded error message as a byte array.
+     */
+    byte[] encodeError(String errorMessage);
+
+    /**
+     * Encodes an integer value into the RESP protocol format.
+     *
+     * @param value The integer value to encode.
+     * @return The encoded integer as a byte array.
+     */
+    byte[] encodeInteger(long value);
+
+    /**
+     * Encodes a bulk string into the RESP protocol format.
+     *
+     * @param bulkString The bulk string to encode.
+     * @return The encoded bulk string as a byte array.
+     */
+    byte[] encodeBulkString(byte[] bulkString);
+
+    /**
+     * Encodes a simple string into the RESP protocol format.
+     *
+     * @param simpleString The simple string to encode.
+     * @return The encoded simple string as a byte array.
+     */
+    byte[] encodeSimpleString(byte[] simpleString);
 }
