d6a0c9037dc1a1bb1b9978f97739aa6334d7df51
namphn <hoangnamuet.vnu@gmail.com>
2025-07-22 08:30:11 +0700

    feat: add incr command

---
+++ src/main/java/dev/namph/redis/cmd/impl/IncrCommand.java
@@ -0,0 +1,52 @@
+package dev.namph.redis.cmd.impl;
+
+
+import dev.namph.redis.cmd.Cmd;
+import dev.namph.redis.cmd.NeedsStore;
+import dev.namph.redis.cmd.RedisCommand;
+import dev.namph.redis.net.Connection;
+import dev.namph.redis.resp.ProtocolEncoder;
+import dev.namph.redis.store.IStore;
+import dev.namph.redis.util.Singleton;
+
+import java.util.List;
+
+@Cmd(name = "INCR", minArgs = 2)
+public class IncrCommand implements RedisCommand, NeedsStore {
+    private IStore store;
+    private final ProtocolEncoder encoder = Singleton.getResp2Encoder();
+
+    @Override
+    public void setStore(IStore store) {
+        this.store = store;
+    }
+
+    @Override
+    public byte[] execute(Connection connection, List<byte[]> argv) {
+        if (argv.size() != 2) {
+            return encoder.encodeError("ERR wrong number of arguments for 'incr' command");
+        }
+
+        byte[] key = argv.get(1);
+        byte[] valueBytes = store.get(key);
+        long value;
+
+        if (valueBytes == null) {
+            // If the key does not exist, initialize it to 0
+            value = 0;
+        } else {
+            try {
+                value = Long.parseLong(new String(valueBytes));
+            } catch (NumberFormatException e) {
+                return encoder.encodeError("ERR value is not an integer or out of range");
+            }
+        }
+
+        // Increment the value
+        value++;
+        store.set(key, String.valueOf(value).getBytes());
+
+        // Return the incremented value as a simple string
+        return encoder.encodeInteger(value);
+    }
+}
