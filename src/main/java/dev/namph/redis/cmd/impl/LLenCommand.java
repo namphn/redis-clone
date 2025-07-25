f48994b26e793d4124d14bdefd622106c333cf48
namphn <hoangnamuet.vnu@gmail.com>
2025-07-25 09:10:15 +0700

    feat: add llen command

---
+++ src/main/java/dev/namph/redis/cmd/impl/LLenCommand.java
@@ -0,0 +1,49 @@
+package dev.namph.redis.cmd.impl;
+
+import dev.namph.redis.cmd.Cmd;
+import dev.namph.redis.cmd.NeedsStore;
+import dev.namph.redis.cmd.RedisCommand;
+import dev.namph.redis.net.Connection;
+import dev.namph.redis.resp.ProtocolEncoder;
+import dev.namph.redis.store.IStore;
+import dev.namph.redis.store.impl.Key;
+import dev.namph.redis.store.impl.QuickList;
+
+import java.util.List;
+
+@Cmd(name = "LLEN", minArgs = 2)
+public class LLenCommand implements RedisCommand, NeedsStore {
+    IStore store;
+    ProtocolEncoder encoder;
+
+    @Override
+    public void setStore(IStore store) {
+        this.store = store;
+    }
+
+    @Override
+    public void setEncoder(ProtocolEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    @Override
+    public byte[] execute(Connection connection, List<byte[]> argv) {
+        if (argv.size() < 2) {
+            return encoder.encodeError("ERR wrong number of arguments for 'LLEN' command");
+        }
+
+        var key = new Key(argv.get(1));
+        var value = store.get(key);
+
+        if (value == null) {
+            return encoder.encodeInteger(0);
+        }
+
+        if (!(value instanceof QuickList list)) {
+            return encoder.encodeError("WRONG TYPE Operation against a key holding the wrong kind of value");
+        }
+
+        // Return the length of the list
+        return encoder.encodeInteger(list.size());
+    }
+}
