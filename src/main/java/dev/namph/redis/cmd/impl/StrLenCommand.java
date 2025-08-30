156cc44046096772102a50ef54d19051288ec4bb
namphn <hoangnamuet.vnu@gmail.com>
2025-08-30 14:14:37 +0700

    feat: add strlen command

---
+++ src/main/java/dev/namph/redis/cmd/impl/StrLenCommand.java
@@ -0,0 +1,40 @@
+package dev.namph.redis.cmd.impl;
+
+import dev.namph.redis.cmd.Cmd;
+import dev.namph.redis.cmd.NeedsStore;
+import dev.namph.redis.cmd.RedisCommand;
+import dev.namph.redis.net.Connection;
+import dev.namph.redis.resp.ProtocolEncoder;
+import dev.namph.redis.store.IStore;
+import dev.namph.redis.store.impl.RedisString;
+
+import java.util.List;
+
+@Cmd(name = "STRLEN", minArgs = 2)
+public class StrLenCommand implements RedisCommand, NeedsStore {
+    private IStore store;
+    private ProtocolEncoder encoder;
+
+
+    @Override
+    public void setStore(IStore store) {
+        this.store = store;
+    }
+
+    @Override
+    public void setEncoder(ProtocolEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    @Override
+    public byte[] execute(Connection connection, List<byte[]> argv) {
+        if (argv.size() != 2) encoder.encodeError("ERR wrong number of arguments for 'STRLEN' command");
+
+        var val = store.get(argv.get(1));
+        if (val == null) return encoder.encodeInteger(0);
+        if (val instanceof RedisString redisString) {
+            return encoder.encodeInteger(redisString.getStringValue().length());
+        }
+        return encoder.encodeError("WRONGTYPE Operation against a key holding the wrong kind of value");
+    }
+}
